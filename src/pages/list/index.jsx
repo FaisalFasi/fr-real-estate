50db65ea9afaba29753dd51ad364d33fdc2ec991
faisalrehman <faisalfasi18@gmail.com>
2024-03-28 18:22:35 +0100

    filter, card component and list page added

--- src/pages/list/index.jsx
+++ src/pages/list/index.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./list.scss";
+import Filter from "../../components/Filter/Filter";
+import { listData } from "../../lib/dummydata";
+import Card from "../../components/Card/Card";
 
 const List = () => {
-  return <div>List Page</div>;
+  const data = listData;
+
+  return (
+    <div className="list">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => {
+            return <Card key={item.id} item={item} />;
+          })}
+        </div>
+      </div>
+      <div className="mapContainer">Map</div>
+    </div>
+  );
 };
 
 export default List;
