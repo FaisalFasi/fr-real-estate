9130e2b91ca429249aa1b812e49ca0245f725d36
faisalrehman <faisalfasi18@gmail.com>
2024-04-01 19:29:02 +0200

    add basic api flow

--- api/app.js
+++ api/app.js
@@ -4,6 +4,8 @@ import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(8800, () => {
--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,4 +1,17 @@
+import bcrypt from "bcrypt";
+
 export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  // HASH THE PASSWORD
+
+  const hashedPassword = bcrypt.hash(password, 10);
+
+  // CREATE A NEW USER
+  // SAVE THE USER
+  // SEND THE USER
+
+  console.log(req.body);
   res.send("register");
   console.log("register");
 };
