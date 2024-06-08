807056b3f1c6784dabdb5bca994658987fb91d35
faisalrehman <faisalfasi18@gmail.com>
2024-06-08 13:29:47 +0200

    Initial commit or your message

--- api/lib/socket.js
+++ api/lib/socket.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 const setupSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: true,
+      origin: process.env.CLIENT_URL || "http://localhost:5173", // Replace with your client domain
       methods: ["GET", "POST"],
       credentials: true,
     },
