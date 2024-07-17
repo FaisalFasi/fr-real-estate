7e0b9f96c01256ac9d2472645a589a827c5aa63a
faisalrehman <faisalfasi18@gmail.com>
2024-07-17 14:01:39 +0200

    deployment test 012, updated cookie settings

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -60,29 +60,15 @@ export const login = async (req, res) => {
 
     const { password: userPassword, ...userInfo } = user;
 
-    const allowedDomains = [
-      "https://fr-real-estate-1.onrender.com",
-      "https://fr-real-estate-1.onrender.com/",
-      "fr-real-estate-1.onrender.com",
-      "fr-real-estate-1.onrender.com/",
-      "onrender.com",
-      "localhost:5173",
-    ];
-    const cookieDomain =
-      process.env.CLIENT_URL || "fr-real-estate-1.onrender.com";
-
-    if (!allowedDomains.includes(cookieDomain)) {
-      console.error(`Invalid domain: ${cookieDomain}`);
-    }
     // Testing 003 - 2021-09-29
 
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // Ensure HTTPS in production
       maxAge: age, // Cookie expiry time
-      sameSite: "none",
+      sameSite: "lax",
       path: "/", // Root path
-      domain: cookieDomain,
+      domain: process.env.CLIENT_URL,
     });
 
     console.log(" cookie domain name ", process.env.CLIENT_URL);
